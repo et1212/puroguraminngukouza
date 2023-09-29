455ec5896282c7a4201f04735c70834b2fb68dd1
et1212 <ento20021212@gmail.com>
2023-09-30 02:05:07 +0900

    ブログ投稿作成(保存)処理実行

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Route::get('/', function () {
 Route::get('/posts', [PostController::class, 'index']);
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
